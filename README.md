7c487b43d0961df86ca13593472447983dd2fd22
hexueyuan <im.hexueyuan@outlook.com>
2018-12-21 00:23:18 +0800

    update hat and README

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 # chatbot
-微信表情包机器人
+微信机器人
+基于itchat的微信机器人框架，把自己的微信号变成一个智能机器人。
+
+---
 
 ## 项目介绍  
-本项目基于[itchat](https://github.com/littlecodersh/itchat)库进行开发，使用爬虫访问[bee-ji](http://www.bee-ji.com)网站获取表情包，仅娱乐非商用。  
+本项目基于[itchat](https://github.com/littlecodersh/itchat)库进行开发，启动之后机器人监听私聊和群聊消息，当匹配文字指令之后，执行机器人服务。  
+
+---
+目前实现了两个服务：  
+ 1. 微信表情包  
+使用爬虫访问[bee-ji](http://www.bee-ji.com)网站获取表情包，仅娱乐非商用。  
 项目后台启动后监听登录账号收发的消息，并捕捉如下指令。  
 > 表情包  
 > 表情包：关键词  
 > 表情包:关键词  
 
-支持群聊@账号加命令。  
-使用见下图：
-![群聊中使用](https://github.com/hexueyuan/chatbot/blob/master/static/qunliao.jpg)
-![私聊中使用](https://github.com/hexueyuan/chatbot/blob/master/static/siliao.jpg)
+群聊消息需要@账号加指令。  
+使用见下图:
+![群聊中使用](/static/qunliao.jpg)  
+
+![私聊中使用](/static/siliao.jpg)
+
+为了防止用户恶意某些不当到关键词，添加了黑名单词表，当触发黑名单词时，返回自定义图片。
+static目录下blackword为黑名单词表，每一行为一个黑名单词，当用户关键词匹配黑名单词时，输出static/warning.gif图片，当用户关键词没有找到相关图片时输出static/noimage.jpg，以上均可根据喜好修改。
+
+2. 我要圣诞帽
+基于dlib的人脸识别和opencv的图像处理，微信机器人监听好友私聊信息，识别到`我要圣诞帽`指令时，获取好友头像并添加圣诞帽，然后返回合成后到图片给好友。
+![我要圣诞帽](/static/chrismashat.jpg)
+
+---
 
 ## 下载
 > git clone git@github.com:hexueyuan/chatbot.git
@@ -20,16 +38,57 @@
 > cd chatbot  
 > . init.sh  
 
-输出当前环境，当init.sh检查结果正常时  
+输出当前环境，当init.sh检查结果正常时(未添加opencv环境检查，请自行检查opencv环境)  
 
 > cd src  
 > python chatbot  
 
 然后会在命令行出现二维码，使用微信扫描登录之后，chatbot开始运行
 
-## tips
-static目录下blackword为黑名单词表，每一行为一个黑名单词，当用户关键词匹配黑名单词时，输出static/warning.gif图片，当用户关键词没有找到相关图片时输出static/noimage.jpg，以上均可根据喜好修改。
+## 开发 
+微信机器人可以很容易扩展其他到服务，比如你可以通过微信机器人传送远端文件，这样你可以通过手机和家里登录了微信机器人的电脑进行文件传送，也可以通过微信机器人开发智能聊天机器人等。  
 
-2018-11-26 更新：
-    重构命令注册方式，在strategy.py文件中四个全局map变量注册命令和命令触发函数，便于扩展
-    新增支持动态添加黑名单词，命令规则为：`黑名单词：关键词`
+微信机器人将添加新服务的过程进行了简单封装，在src/strategy.py程序中，有如下几个dict:
+```python
+#所有用户私聊和群消息均可触发
+#其它用户群消息必须@本用户触发
+strategy_map_global = {
+    "表情包$": send_emoji_chat,
+    "表情包：.*$": send_emoji_chat_with_query
+}
+
+#所有用户群消息
+#其它用户必须@本用户触发
+strategy_map_chatroom = {}
+
+#私聊
+strategy_map_chatone = {
+    "我要圣诞帽$": christmas_hat
+}
+
+#本用户消息触发
+strategy_map_me = {
+    "表情包$": send_emoji_chat,
+    "表情包：.*$": send_emoji_chat_with_query,
+    "黑名单词：.*$": add_blackword
+}
+```
+这是服务注册的配置，若你需要增加一个新的服务，请按照已有的服务函数(send_emoji_chat、christmas_hat)写自己的服务函数，并添加到服务注册配置中，服务注册有四个注册体，分别监控`群聊中其他用户信息`、`私聊中他人的信息`、`群聊和私聊中本用户的信息`、`群聊和私聊中其他用户的信息`,参见代码中的注释部分，注册关键词为正则匹配模式。
+
+以下提供一个开发样例，实现用户私聊发送`我的信息`时返回在itchat中查询到的好友信息。  
+首先编写服务函数，这部分添加到strategy.py文件中:  
+```python
+import json
+
+def send_user_info(msg, isGroup=False):
+    info = itchat.search_friends(msg["FromUserName"]
+    msg.user.send(json.dumps(info), indent=2, ensure_ascii=False))
+```
+函数很简单，当触发该服务时，获取msg中到user信息，并使用json库dump为字符串并返回给好友。接下来在私聊服务注册体中增加配置：  
+```python
+strategy_map_chatone = {
+    "我要圣诞帽$": christmas_hat,
+    "我的信息$": send_user_info
+}
+```
+注册完成之后重新登录chatbot即可。
--- src/hat.py
+++ src/hat.py
@@ -1,128 +1,150 @@
-#!/usr/bin/env python
 # -*- coding:utf8 -*-
 
+#import hat
 import numpy
-import cv2
+import json
 import dlib
+import cv2
+import math
 
 predictor_path = "../static/shape_predictor_5_face_landmarks.dat"
 
+#将图片沿着图片中点逆时针旋转
+def rotate(image, angle, center=None, scale=1.0): #1
+    (h, w) = image.shape[:2] #2
+    if center is None: #3
+        center = (w // 2, h // 2) #4
+
+    M = cv2.getRotationMatrix2D(center, angle, scale) #5
+
+    rotated = cv2.warpAffine(image, M, (w, h)) #6
+    return rotated #7
+
+# 求p2 -> p1与y正半轴的夹角弧度制
+def offset_angle(point1, point2):
+    len = math.sqrt((point1[0] - point2[0]) * (point1[0] - point2[0]) + (point1[1] - point2[1]) * (point1[1] - point2[1]))
+    theta = math.acos((point1[1] - point2[1]) / len)
+    return theta
+
 def add_hat(head_img_path, hat_img_path):
-    """return the path of head with hat
-    """
-    #处理图像，微信头像格式位jpg三通道
     head_img = cv2.imread(head_img_path)
     hat_img = cv2.imread(hat_img_path, -1)
-    
-    #分离rgba通道，合成rgb三通道帽子图，a通道后面做mask用
-    r, g, b, a = cv2.split(hat_img) 
-    rgb_hat = cv2.merge((r,g,b))
 
-    #dlib人脸关键点检测器
+    #加载人脸识别训练模型，检测图片中的人脸图像
     predictor = dlib.shape_predictor(predictor_path)  
-
-    # dlib正脸检测器
     detector = dlib.get_frontal_face_detector()
-    # 正脸检测
     dets = detector(head_img, 1)
-    # 如果检测到人脸
-    if len(dets)>0:  
-        for d in dets:
-            #人脸的左边、人脸的顶部、宽度、高度
-            x,y,w,h = d.left(),d.top(), d.right()-d.left(), d.bottom()-d.top()
-            
-            # 关键点检测，5个关键点
-            shape = predictor(head_img, d)
-           
-            # 选取左右眼眼角的点
-            point1 = shape.part(0)
-            point2 = shape.part(2)
 
-            # 求两点中心
-            eyes_center = ((point1.x+point2.x)//2,(point1.y+point2.y)//2)
-    
-            #  根据人脸大小调整帽子大小
-            factor = 2.5
-            resized_hat_h = int(round(rgb_hat.shape[0]*w/rgb_hat.shape[1]*factor))
-            resized_hat_w = int(round(rgb_hat.shape[1]*w/rgb_hat.shape[1]*factor))
-            if resized_hat_h > y:
-                resized_hat_h = y
-            resized_hat = cv2.resize(rgb_hat,(resized_hat_w,resized_hat_h))
-            #print "根据人脸调整后的帽子size:", resized_hat_h, resized_hat_w
-
-            # 帽子相对与人脸框上线的偏移量
-            dh = 0
-            dw = 0
-
-            # 求帽子的所在区域，根据边框裁剪
-            top = y + dh - resized_hat_h
-            if top < 0:
-                top = 0
-            #print "top:", y + dh - resized_hat_h, top
-
-            bottom = y + dh
-            if bottom >= head_img.shape[0]:
-                bottom = head_img.shape[0] - 1
-            #print "bottom:", y + dh - 1, bottom
-            
-            left = eyes_center[0]-resized_hat_w/2
-            if left < 0:
-                left = 0
-            #print "left:", eyes_center[0]-resized_hat_w/2, left
-            
-            right = eyes_center[0]+resized_hat_w/2
-            if right >= head_img.shape[1]:
-                right = head_img.shape[1] - 1
-            #print "right:", eyes_center[0]+resized_hat_w/2, right
-
-            bg_roi = head_img[top:bottom, left:right]
-            #cv2.imwrite("bg.jpg", bg_roi)
-            #print "bg:", top, bottom, left, right
-            #bg_w = right - left
-            #bg_h = bottom - top
-
-            #修剪帽子
-            cut_hat = resized_hat[top - (y + dh - resized_hat_h):bottom, (left - (eyes_center[0]-resized_hat_w/2)):(resized_hat_w - (right - (eyes_center[0]+resized_hat_w/2)))]
-            #print top - (y + dh - resized_hat_h), bottom, (left - (eyes_center[0]-resized_hat_w/2)), (resized_hat_w - (right - (eyes_center[0]+resized_hat_w/2)))
-
-            # 用alpha通道作为mask
-            a = cv2.resize(a, (resized_hat_w,resized_hat_h))
-            a = a[top - (y + dh - resized_hat_h):bottom, (left - (eyes_center[0]-resized_hat_w/2)):(resized_hat_w - (right - (eyes_center[0]+resized_hat_w/2)))]
-            mask = cv2.resize(a, (cut_hat.shape[1] ,cut_hat.shape[0]))
-            mask_inv =  cv2.bitwise_not(mask)
-
-            # 原图ROI中提取放帽子的区域
-            bg_roi = bg_roi.astype(float)
-            mask_inv = cv2.merge((mask_inv,mask_inv,mask_inv))
-            alpha = mask_inv.astype(float)/255
-
-            # 相乘之前保证两者大小一致（可能会由于四舍五入原因不一致）
-            alpha = cv2.resize(alpha, (bg_roi.shape[1],bg_roi.shape[0]))
-            bg = cv2.multiply(alpha, bg_roi)
-            bg = bg.astype('uint8')
-            # cv2.imwrite("bg.jpg",bg)
-
-            # 提取帽子区域
-            hat = cv2.bitwise_and(cut_hat,cut_hat,mask = mask)
-            # cv2.imwrite("hat.jpg",hat)
-
-            # 相加之前保证两者大小一致（可能会由于四舍五入原因不一致）
-            hat = cv2.resize(hat,(bg_roi.shape[1],bg_roi.shape[0]))
-            # 两个ROI区域相加
-            add_hat = cv2.add(bg, hat)
-            cv2.imwrite("merge_hat.jpg", add_hat)
-
-            # 把添加好帽子的区域放回原图
-            head_img[top:bottom, left:right] = add_hat
+    if len(dets) > 0:
+        head_img = add_hat_to_face(head_img, hat_img, predictor, dets)
+        head_with_hat_img_path = head_img_path.replace('.jpg', '') + '_with_hat.jpg'
+        cv2.imwrite(head_with_hat_img_path, head_img)
+        return head_with_hat_img_path
     else:
         return None
-    head_with_hat_img_path = head_img_path.replace('.jpg', '') + '_with_hat.jpg'
-    cv2.imwrite(head_with_hat_img_path, head_img)
-    return head_with_hat_img_path
 
+def add_hat_to_face(head_img, hat_img, predictor, dets):
+    """return the path of head with hat
+    """
+    for d in dets:
+        # 关键点检测，5个关键
+        shape = predictor(head_img, d)
+
+        # 计算帽子随人脸的偏转角度
+        # 右眼的中点
+        mid1 = ((shape.part(0).x + shape.part(1).x) / 2, (shape.part(0).y + shape.part(1).y) / 2)
+        # 左眼的中点
+        mid2 = ((shape.part(2).x + shape.part(3).x) / 2, (shape.part(2).y + shape.part(3).y) / 2)
+        theta = offset_angle(mid2, mid1)
+        real_angle = int((math.pi * 2.5 - theta) * 180 / math.pi)
+        ex_hat = rotate(hat_img, real_angle)
+
+        # 根据人脸大小调整帽子大小, 人脸大小由双眼眼角距离决定
+        leye = shape.part(0)
+        reye = shape.part(2)
+        eye_distance = math.sqrt((leye.x - reye.x) * (leye.x - reye.x) + (leye.y - reye.y) * (leye.y - reye.y))
+        # k1参数表示帽子图片相对人脸的大小
+        k1 = 2.3 * 3
+        face_width = eye_distance
+        resized_hat_w = int(face_width * k1)
+        resized_hat_h = int(hat_img.shape[0] * resized_hat_w / hat_img.shape[1])
+        resized_hat = cv2.resize(ex_hat,(resized_hat_w,resized_hat_h))
+
+        # 求解帽子相对人脸的垂直偏移
+        # 系数k2表示帽子到双眼所在水平线的距离为双眼所在水平线到门中的距离的k倍
+        k2 = 1.6
+
+        #门中到双眼线的距离, 这里简化为求门中到双眼中点的距离
+        mid_eye = ((mid1[0] + mid2[0]) / 2, (mid1[1] + mid2[1]) / 2)
+        point_nose = shape.part(4)
+        distance = math.sqrt((mid_eye[0] - point_nose.x) * (mid_eye[0] - point_nose.x) + (mid_eye[1] - point_nose.y) * (mid_eye[1] - point_nose.y))
+
+        # 求帽子在头像图片中的位置
+        angle_offset = math.pi - theta
+        img_bottom = int((d.top() + (resized_hat.shape[0] / 2) - (k2 * distance * math.sin(angle_offset) - (mid_eye[1] - d.top()))))
+        img_top = int(img_bottom - resized_hat.shape[0])
+        img_left = int(k2 * distance * math.cos(angle_offset) + mid_eye[0] - 0.5 * resized_hat.shape[1])
+        img_right = int(img_left + resized_hat.shape[1])
+        print img_left, img_right
+        print img_top, img_bottom
+        #裁剪图片
+        if img_left < 0:
+            delta_left = 0 - img_left
+            img_left = 0
+        else:
+            delta_left = 0
+        if img_right > head_img.shape[1]:
+            delta_right = img_right - head_img.shape[1]
+            img_right = head_img.shape[1] - 1
+        else:
+            delta_right = 0
+        if img_top < 0:
+            deleta_top = 0 - img_top
+            img_top = 0
+        else:
+            deleta_top = 0
+        #默认bottom不会超过头像图片
+        bg_roi = head_img[img_top:img_bottom, img_left:img_right]
+        hat_roi = resized_hat[deleta_top:resized_hat.shape[0], delta_left:resized_hat.shape[1] - delta_right]
+        r, g, b, a = cv2.split(hat_roi)
+        rgb_hat_roi = cv2.merge((r, g, b))
+
+        mask_inv =  cv2.bitwise_not(a)
+
+        ## 原图ROI中提取放帽子的区域
+        bg_roi = bg_roi.astype(float)
+        mask_inv = cv2.merge((mask_inv,mask_inv,mask_inv))
+        alpha = mask_inv.astype(float)/255
+
+        ## 相乘之前保证两者大小一致（可能会由于四舍五入原因不一致）
+        alpha = cv2.resize(alpha, (bg_roi.shape[1],bg_roi.shape[0]))
+        print alpha.shape
+        print bg_roi.shape
+        bg = cv2.multiply(alpha, bg_roi)
+        bg = bg.astype('uint8')
+        ## cv2.imwrite("bg.jpg",bg)
+
+        ## 提取帽子区域
+        hat = cv2.bitwise_and(rgb_hat_roi, rgb_hat_roi, mask = a)
+        ## cv2.imwrite("hat.jpg",hat)
+
+        ## 相加之前保证两者大小一致（可能会由于四舍五入原因不一致）
+        hat = cv2.resize(hat,(bg_roi.shape[1],bg_roi.shape[0]))
+        ## 两个ROI区域相加
+        add_hat = cv2.add(bg, hat)
+
+        ## 把添加好帽子的区域放回原图
+        head_img[img_top:img_bottom, img_left:img_right] = add_hat
+    return head_img
 
 if __name__ == "__main__":
-    import sys
-    print "python hat.py head_image hat_image..."
-    print sys.argv[1], sys.argv[2]
-    print add_hat(sys.argv[1], sys.argv[2])
+    test_head_img = []
+    path = "../test/head_img/"
+    hat_img = "../static/hat/hat.png"
+    #for i in range(1, 27):
+    #    img = path + str(i) + '.jpg'
+    #    test_head_img.append(img)
+    test_head_img.append(path + "1.jpg")
+
+    for img in test_head_img:
+        print add_hat(img, hat_img)
